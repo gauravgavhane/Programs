d99dec62b0be61af1d7ff2e95e10230c36981011
Gaurav Gavhane <109721167+gauravgavhane@users.noreply.github.com>
2023-04-24 09:46:52 +0530

    Add files via upload

---
+++ Problems on Pattern Printing/program7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void Display(int iNo)   
+{
+       int iCnt =0;
+       
+       for(iCnt = -iNo; iCnt <= iNo; iCnt++)
+       {
+            printf("%d\t",iCnt);       
+       }
+}
+
+/////////////////////////////////////////////////////////////////////////
+//  Accept number from user and display the pattern
+/////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue =0;
+
+    printf("Please enter the value :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);   
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Input  : 	     4
+// Output :        -4   -3  -2  -1  0   1   2   3   4
+//
+/////////////////////////////////////////////////////////////////////////

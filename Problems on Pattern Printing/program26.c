ff64ac09e6a6d2cfda6fd3e13bbde86552406dea
Gaurav Gavhane <109721167+gauravgavhane@users.noreply.github.com>
2023-05-16 23:11:20 +0530

    Add files via upload

---
+++ Problems on Pattern Printing/program26.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+        int i = 0, j = 0;
+        char ch = '\0';
+
+        for(i = 1,ch = 'A'; i <= iRow; i++,ch++)
+        {
+            for(j = 1; j<= iCol; j++)
+            {
+                printf("%c\t",ch);
+            }
+            printf("\n");
+        }
+}
+
+////////////////////////////////////////////////////////////////////
+//  Accept number from user and display the pattern
+////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : ");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////
+//
+//        Input :  Row 4   Columns  4
+//
+//        A   A   A   A
+//        B   B   B   B
+//        C   C   C   C
+//        D   D   D   D
+//
+////////////////////////////////////////////////////////////////////

b3d4992837237319f542e68dc5e3a7afe1be85e3
Gaurav Gavhane <109721167+gauravgavhane@users.noreply.github.com>
2023-05-18 01:24:56 +0530

    Add files via upload

---
+++ Problems on Pattern Printing/program34.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+        int i = 0, j = 0;
+
+        if(iRow != iCol)
+        {
+            return;
+        }
+        
+         for(i = 1; i <= iRow; i++)
+        {
+            for(j = iCol; j>=1; j--)
+            {
+                    if(i == j)
+                    {
+                            printf("$\t");
+                    }
+                    else
+                    {
+                          printf("*\t");
+                    }
+            }
+            printf("\n");
+        }
+}
+
+////////////////////////////////////////////////////////////////////
+//  Accept number from user and display the pattern
+////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : ");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////
+//
+//            Input :  Row 4   Columns    4
+//
+//            *   *   *   $
+//            *   *   $   *
+//            *   $   *   *
+//            $   *   *   *
+//
+////////////////////////////////////////////////////////////////////

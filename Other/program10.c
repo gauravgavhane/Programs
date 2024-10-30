a6bffe3a1eb9a7db3b13133f38d10d6555d793be
Gaurav Gavhane <109721167+gauravgavhane@users.noreply.github.com>
2024-10-31 05:14:01 +0530

    Add files via upload

---
+++ Other/program10.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Grade
+//  Description :   Used to display grades based on marks
+//  Input :         Integer
+//  Output :        Void
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+void Grade(int Marks)
+{
+    if ((Marks < 0) &&  (Marks> 100))
+    {
+        printf("Invalid marks\n");
+        return;
+    }
+    
+    if (Marks >= 95)
+    {
+        printf("A+ grade\n");
+    }
+    else if ((Marks >= 90) && (Marks < 95))
+    {
+        printf("A grade\n");
+    }
+    else if ((Marks >= 85) && (Marks < 90))
+    {
+        printf("B+ grade\n");
+    }
+    else if ((Marks >= 80) && (Marks < 85))
+    {
+        printf("B grade\n");
+    }
+    else if ((Marks >= 75) && (Marks < 80))
+    {
+        printf("C+ grade\n");
+    }
+    else if ((Marks >= 70) && (Marks < 75))
+    {
+        printf("C grade\n");
+    }
+    else if ((Marks >= 35) && (Marks < 70))
+    {
+        printf("Pass\n");
+    }
+    else
+    {
+        printf("Fail\n");
+    }
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////
+// Accept marks from user & display the grade 
+//////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter your marks : ");
+    scanf("%d",&iValue);
+
+    Grade(iValue);
+
+    return 0;
+}
+
+/*/////////////////////////////////////////////////////////////////////////////////////
+
+Input  : 23
+Output : Fail
+
+Input  : -10
+Output : Invalid
+
+Input  : 94
+Output : A
+
+/*/////////////////////////////////////////////////////////////////////////////////////

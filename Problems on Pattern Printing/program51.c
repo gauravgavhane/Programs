af8c343312fb9b407c290d6f3c197599ef0027e5
Gaurav Gavhane <109721167+gauravgavhane@users.noreply.github.com>
2024-11-14 20:23:12 +0530

    Add files via upload

---
+++ Problems on Pattern Printing/program51.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   Used to print the pattern
+//  Input :         Integer
+//  Output :        Void
+//
+////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Number of rows and columns are not same \n");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j<= iCol; j++)
+        {
+            if( i <= j)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// Accept number of rows and number of columns from user and display below pattern
+///////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+/*//////////////////////////////////////////////////////////////////////////////
+
+Input  :    iRow = 4    iCol = 4
+
+Output :    
+            1	2	3	4	
+                2	3	4	
+                    	3	4	
+                        	4
+
+*////////////////////////////////////////////////////////////////////////////////
+
